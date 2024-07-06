300bf34569c61d9fd6df83289f4f1ea674394072
Antal Tony Tettinger <antal.tettinger@gmail.com>
2024-07-06 14:46:45 +0200

    autocomplete and snippets

--- lua/plugins/neo-tree.lua
+++ lua/plugins/neo-tree.lua
@@ -1,13 +1,25 @@
 return {
-  {    "nvim-neo-tree/neo-tree.nvim",
-    branch = "v3.x",
+  {
+    'nvim-neo-tree/neo-tree.nvim',
+    branch = 'v3.x',
     dependencies = {
-      "nvim-lua/plenary.nvim",
-      "nvim-tree/nvim-web-devicons",
-      "MunifTanjim/nui.nvim",
-   },
-   config = function()
-   vim.keymap.set('n', '<C-n>', ':Neotree filesystem reveal left toggle<CR>' , {})
-   end
-}  
+      'nvim-lua/plenary.nvim',
+      'nvim-tree/nvim-web-devicons',
+      'MunifTanjim/nui.nvim',
+    },
+    config = function()
+      require('neo-tree').setup {
+        event_handlers = {
+
+          {
+            event = 'file_open_requested',
+            handler = function()
+              require('neo-tree.command').execute { action = 'close' }
+            end,
+          },
+        },
+      }
+      vim.keymap.set('n', '<C-n>', ':Neotree filesystem reveal left toggle<CR>', {})
+    end,
+  },
 }
--- lua/plugins/treesitter.lua
+++ lua/plugins/treesitter.lua
@@ -4,7 +4,7 @@ return
     config = function()
       local configs = require("nvim-treesitter.configs")
       configs.setup({
-          ensure_installed = { "c", "lua", "vim", "vimdoc", "query", "elixir", "heex", "javascript", "html" },
+          auto_install = true,
           sync_install = false,
           highlight = { enable = true },
           indent = { enable = true },
