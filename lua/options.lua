7f2e6d4aa8016c92d85d6a3e2a190b460a4c9f40
Antal Tony Tettinger <antal.tettinger@gmail.com>
2024-07-08 23:21:26 +0200

    updated tmux and harpoon

--- lua/options.lua
+++ lua/options.lua
@@ -1,20 +1,39 @@
+vim.api.nvim_set_option("clipboard", "unnamed")
 vim.cmd 'set expandtab'
 vim.cmd 'set tabstop=2'
 vim.cmd 'set softtabstop=2'
 vim.cmd 'set shiftwidth=2'
 vim.g.mapleader = " "
 
-vim.keymap.set('n', '<c-;>', ':wincmd ;<CR>')
-vim.keymap.set('n', '<c-j>', ':wincmd j<CR>')
-vim.keymap.set('n', '<c-k>', ':wincmd k<CR>')
-vim.keymap.set('n', '<c-l>', ':wincmd l<CR>')
+vim.keymap.set({'v', 's','n'}, '<c-;>', ':wincmd i<CR>')
+vim.keymap.set({'v', 's','n'}, '<c-j>', ':wincmd j<CR>')
+vim.keymap.set({'v', 's','n'}, '<c-k>', ':wincmd k<CR>')
+vim.keymap.set({'v', 's','n'}, '<c-l>', ':wincmd l<CR>')
 
-vim.keymap.set('n', ';', 'l')
-vim.keymap.set('n', 'l', 'k')
-vim.keymap.set('n', 'k', 'j')
-vim.keymap.set('n', 'j', 'h')
+vim.keymap.set({'v','s','n'}, 'l', 'k')
+vim.keymap.set({'v','s','n'}, 'k', 'j')
+vim.keymap.set({'v','s','n'}, 'j', 'h')
 
-vim.keymap.set('v', ';', 'l')
-vim.keymap.set('v', 'l', 'k')
-vim.keymap.set('v', 'k', 'j')
-vim.keymap.set('v', 'j', 'h')
+vim.keymap.set('n','<c-d>', '<c-d>zz')
+vim.keymap.set('n','<c-u>', '<c-u>zz')
+vim.keymap.set('n','n', 'nzz')
+vim.keymap.set('n','N', 'Nzz')
+vim.keymap.set('n', '<leader>p', '\"_dP')
+
+-- Enable line numbers
+vim.opt.number = true
+
+-- Enable relative line numbers
+vim.opt.relativenumber = true
+
+
+-- Disable default tmux-navigator mappings
+vim.g.tmux_navigator_no_mappings = 1
+
+-- Define custom mappings
+vim.api.nvim_set_keymap('n', '<C-j>', ':<C-U>TmuxNavigateLeft<CR>', { silent = true })
+vim.api.nvim_set_keymap('n', '<C-k>', ':<C-U>TmuxNavigateDown<CR>', { silent = true })
+vim.api.nvim_set_keymap('n', '<C-l>', ':<C-U>TmuxNavigateUp<CR>', { silent = true })
+vim.api.nvim_set_keymap('n', '<C-;>', ':<C-U>TmuxNavigateRight<CR>', { silent = true })
+vim.api.nvim_set_keymap('n', '<C-p>', ':<C-U>TmuxNavigatePrevious<CR>', { silent = true })
+
