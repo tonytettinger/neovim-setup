89bf8f59c8ad0c137ec95677b1469d2eb4b01ef1
Antal Tony Tettinger <antal.tettinger@gmail.com>
2024-08-07 11:23:07 +0200

    update treesitter

--- lua/plugins/treesitter.lua
+++ lua/plugins/treesitter.lua
@@ -1,21 +1,23 @@
 return {
-  {"nvim-treesitter/nvim-treesitter", 
-    build = ":TSUpdate",
+  {
+    'nvim-treesitter/nvim-treesitter',
+    build = ':TSUpdate',
     config = function()
-      local configs = require("nvim-treesitter.configs")
-      configs.setup({
-          auto_install = true,
-          sync_install = false,
-          highlight = { enable = true },
-          indent = { enable = true },
-          autotag = { enable = true }, -- Enable nvim-ts-autotag
-        })
-    end
+      local configs = require 'nvim-treesitter.configs'
+      configs.setup {
+        auto_install = true,
+        sync_install = false,
+        highlight = { enable = true },
+        indent = { enable = true },
+      }
+    end,
   },
 
   -- Auto-close tags in JSX/HTML
-  { 'windwp/nvim-ts-autotag', config = function()
+  {
+    'windwp/nvim-ts-autotag',
+    config = function()
       require('nvim-ts-autotag').setup()
-    end }
+    end,
+  },
 }
-
